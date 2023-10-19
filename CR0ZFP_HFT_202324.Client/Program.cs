a9b1bbffc0cd8adcf23824c3e0afc4dc0008a77b
Lőrincz Gergő <Lrincz Gerg@DESKTOP-ORR50AE>
2023-10-19 21:03:18 +0200

    Add project files.

---
+++ CR0ZFP_HFT_202324.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CR0ZFP_HFT_202324.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CR0ZFP_HFT_202324.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CR0ZFP_HFT_202324.Test
+{
+    public class Class1
+    {
+    }
+}
