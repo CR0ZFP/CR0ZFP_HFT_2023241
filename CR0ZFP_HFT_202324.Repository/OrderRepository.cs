aa410e7fe32bc66cf060bbc72299547172ec11e2
CR0ZFP <gergolorincz100@gmail.com>
2023-11-15 16:51:42 +0100

    OrderRepository created.

---
+++ CR0ZFP_HFT_202324.Repository/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CR0ZFP_HFT_202324.Models;
+
+namespace CR0ZFP_HFT_202324.Repository
+{
+    public class OrderRepository : Repository<Order>, IRepository<Order>
+    {
+        public OrderRepository(WebshopDbContext ctx): base(ctx)
+        { }
+        public override Order Read(int id)
+        {
+            return this.ctx.Orders.First(t => t.OrderID == id);
+        }
+
+        public override void Update(Order item)
+        {
+            var oldOrder = Read(item.OrderID);
+            foreach (var prop in oldOrder.GetType().GetProperties())
+            {
+                prop.SetValue(oldOrder, prop.GetValue(item));
+            }
+        }
+    }
+}
