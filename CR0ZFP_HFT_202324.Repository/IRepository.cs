3104bfacb6f973e5716dad5c266fa5df253a6c7f
CR0ZFP <gergolorincz100@gmail.com>
2023-11-15 16:24:38 +0100

    Generic Repository interface created.

---
+++ CR0ZFP_HFT_202324.Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR0ZFP_HFT_202324.Repository
+{
+    public interface IRepository <T> where T : class
+    {
+        void Create(T item);
+        IQueryable<T> ReadAll ();
+        T Read(int id);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
