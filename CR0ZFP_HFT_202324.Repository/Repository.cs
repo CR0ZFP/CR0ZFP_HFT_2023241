68d41c81f934691339b1a361635853f85b3f9d88
CR0ZFP <gergolorincz100@gmail.com>
2023-11-15 16:39:48 +0100

    Generic Repository class created.

---
+++ CR0ZFP_HFT_202324.Repository/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR0ZFP_HFT_202324.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected WebshopDbContext ctx;
+
+        public Repository(WebshopDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+        public abstract T Read(int id);
+
+        public abstract void Update(T item);
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+       
+    }
+}
