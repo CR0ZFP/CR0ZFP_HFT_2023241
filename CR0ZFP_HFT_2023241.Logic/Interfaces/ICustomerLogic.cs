72a9e02a2677b02095119004b6e827771954af46
CR0ZFP <gergolorincz100@gmail.com>
2023-11-22 18:39:19 +0100

    Interface created for the CustomerLogic.

---
+++ CR0ZFP_HFT_2023241.Logic/Interfaces/ICustomerLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CR0ZFP_HFT_202324.Models;
+
+namespace CR0ZFP_HFT_2023241.Logic.Interfaces
+{
+    public interface ICustomerLogic
+    {
+        double AvarageAgeOfCustomers();
+        void Create(Customer item);
+        void Delete(int id);
+        List<Customer> GetCustomersByCounty(string County);
+        Customer Read(int id);
+        IQueryable<Customer> ReadAll();
+        void Update(Customer item);
+    }
+}
