82a7aad9de6cae5feb8b67f42081a637608843e7
CR0ZFP <gergolorincz100@gmail.com>
2023-11-22 18:39:35 +0100

    Interface created for the OrderLogic.

---
+++ CR0ZFP_HFT_2023241.Logic/Interfaces/IOrderLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CR0ZFP_HFT_202324.Models;
+
+namespace CR0ZFP_HFT_2023241.Logic.Interfaces
+{
+    internal interface IOrderLogic
+    {
+        void Create(Order item);
+        void Delete(int id);
+        List<Order> GetOrdersByDate(DateTime Date, int option);
+        double GetFulPrice(int Id);
+        double GetFullWeight(int Id);
+        Order Read(int id);
+        IQueryable<Order> ReadAll();
+        void Update(Order item);
+    }
+}
