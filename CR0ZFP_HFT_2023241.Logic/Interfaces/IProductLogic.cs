82a7c8bf21cbcdfee8a659ab87155b3dcce15753
CR0ZFP <gergolorincz100@gmail.com>
2023-11-22 18:40:51 +0100

    Interface created for the ProductLogic.

---
+++ CR0ZFP_HFT_2023241.Logic/Interfaces/IProductLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using CR0ZFP_HFT_202324.Models;
+
+namespace CR0ZFP_HFT_2023241.Logic.Interfaces
+{
+    public interface IProductLogic
+    {
+        void Create(Product item);
+        void Delete(int id);
+        double GetAvaragePrice();
+        Product Read(int id);
+        IQueryable<Product> ReadAll();
+        void Update(Product item);
+    }
+}
