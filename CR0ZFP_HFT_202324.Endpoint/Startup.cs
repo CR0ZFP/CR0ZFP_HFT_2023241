c6a156c094aa77c669aa351dd0b05cbfb26ef08f
CR0ZFP <gergolorincz100@gmail.com>
2023-11-29 20:29:27 +0100

    Startup specified

--- CR0ZFP_HFT_202324.Endpoint/Startup.cs
+++ CR0ZFP_HFT_202324.Endpoint/Startup.cs
@@ -2,20 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CR0ZFP_HFT_202324.Models;
+using CR0ZFP_HFT_202324.Repository;
+using CR0ZFP_HFT_2023241.Logic;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CR0ZFP_HFT_202324.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<WebshopDbContext>();
+
+            services.AddTransient<IRepository<Customer>,CustomerRepository>();
+            services.AddTransient<IRepository<Order>,OrderRepository>();
+            services.AddTransient<IRepository<Product>,ProductRepository>();
+
+
+            services.AddTransient<ICustomerLogic, CustomerLogic>();
+            services.AddTransient<IOrderLogic, OrderLogic>();
+            services.AddTransient<IProductLogic, ProductLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c=> { c.SwaggerDoc("v1", new OpenApiInfo { Title = "CR0ZFP_HFT_202324.Endpoint", Version = "v1" }); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +49,27 @@ namespace CR0ZFP_HFT_202324.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CR0ZFP_HFT_202324.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
