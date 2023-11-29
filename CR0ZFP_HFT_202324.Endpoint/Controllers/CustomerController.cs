3409855ac55d0d55ae3fe2b169dc87b97828510b
CR0ZFP <gergolorincz100@gmail.com>
2023-11-29 20:29:41 +0100

    CustomerController created

---
+++ CR0ZFP_HFT_202324.Endpoint/Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using CR0ZFP_HFT_202324.Models;
+using CR0ZFP_HFT_2023241.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CR0ZFP_HFT_202324.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        ICustomerLogic logic;
+
+        public CustomerController(ICustomerLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET: 
+        [HttpGet]
+        public IEnumerable<Customer> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        // GET 
+        [HttpGet("{id}")]
+        public Customer Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        // POST 
+        [HttpPost]
+        public void Create([FromBody] Customer value)
+        {
+            this.logic.Create(value);
+        }
+
+        // PUT 
+        [HttpPut]
+        public void Update([FromBody] Customer value)
+        {
+            this.Update(value);
+        }
+
+        // DELETE 
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
